a5650401a57dd1292ef2c52894133d3cc1d300d1
Guillaume Lagrange <lagrange.guillaume.1@gmail.com>
2018-12-15 14:10:48 -0500

    Added QPredator methods and attributes

--- PredatorSimulator/PredatorSimulator/QPredator.cpp
+++ PredatorSimulator/PredatorSimulator/QPredator.cpp
@@ -10,3 +10,56 @@ QPredator::QPredator()
 QPredator::~QPredator()
 {
 }
+
+void QPredator::setDamage(int damage)
+{
+	mDamage = damage;
+}
+
+void QPredator::resetTimeNoKill()
+{
+	mTimeNoKill = 0;
+}
+
+void QPredator::incrementTimeNoKill()
+{
+	++mTimeNoKill;
+}
+
+void QPredator::setNextAttributes(int damage, int timeNoKill, qreal scaleFactor)
+{
+	mNextAttributes.damage = damage;
+	mNextAttributes.timeNoKill = timeNoKill;
+	mNextAttributes.scaleFactor = scaleFactor;
+}
+
+int QPredator::getDamage() const
+{
+	return mDamage;
+}
+
+int QPredator::getTimeNoKill() const
+{
+	return mTimeNoKill;
+}
+
+void QPredator::clone()
+{
+}
+
+QRectF QPredator::boundingRect() const
+{
+	return QRectF();
+}
+
+void QPredator::paint(QPainter * painter, const QStyleOptionGraphicsItem * option, QWidget * widget)
+{
+}
+
+void QPredator::advance(int phase)
+{
+}
+
+void QPredator::kill(QDynamicObject * object)
+{
+}
--- PredatorSimulator/PredatorSimulator/QPredator.h
+++ PredatorSimulator/PredatorSimulator/QPredator.h
@@ -2,12 +2,48 @@
 #define Q_PREDATOR_H
 
 #include "QDynamicObject.h"
+#include "Attributes.h"
 
+// La classe QPredator interagit avec 
 class QPredator : public QDynamicObject
 {
 public:
+	// TO-DO: constructeur avec des paramètres (par défaut)
 	QPredator();
 	~QPredator();
+
+	// Mutateurs
+	void setDamage(int damage);
+	void resetTimeNoKill();
+	void incrementTimeNoKill();
+	void setNextAttributes(int damage, int timeNoKill, qreal scaleFactor);
+	
+	// Accesseurs
+	int getDamage() const;
+	int getTimeNoKill() const;
+
+	// Clone (fonction abstraite de QDynamicObject - doit être "overridée")
+	void clone() override;
+
+	// Fonction abstraite de QGraphicsItem (DOIT être "overridée") : retourne la taille de l'item
+	QRectF boundingRect() const override;
+	// Fonction abstraite de QGraphicsItem (DOIT être "overridée") : dessine la forme
+	void paint(QPainter * painter, const QStyleOptionGraphicsItem *option, QWidget * widget) override;
+
+	// Fonction virtuelle de QGraphicsItem (peut être "overridée") : fait un tick de simulation
+	void advance(int phase) override;
+
+private:
+	// Le dommage que le prédateur inflige
+	int mDamage;
+	// Le temps depuis sa dernière victime
+	int mTimeNoKill;
+	// Les prochains attributs
+	PredatorAttributes mNextAttributes;
+
+	// Élimine l'entité avec laquelle elle entre en contact
+	void kill(QDynamicObject* object);
+
 };
 
 #endif // !Q_PREDATOR_H
