06f6a72df6b039ce91967545ff39ff245bceb31d
Guillaume Lagrange <lagrange.guillaume.1@gmail.com>
2018-12-15 14:11:14 -0500

    Added QRunner methods and attributes

--- PredatorSimulator/PredatorSimulator/QRunner.cpp
+++ PredatorSimulator/PredatorSimulator/QRunner.cpp
@@ -10,3 +10,36 @@ QRunner::QRunner()
 QRunner::~QRunner()
 {
 }
+
+void QRunner::setHP(int hp)
+{
+	mHealthPoints = hp;
+}
+
+void QRunner::setNextAttributes(int hp, QBrush const & brush)
+{
+	mNextAttributes.healthPoints = hp;
+	mNextAttributes.brush = brush;
+}
+
+int QRunner::getHP() const
+{
+	return mHealthPoints;
+}
+
+void QRunner::clone()
+{
+}
+
+QRectF QRunner::boundingRect() const
+{
+	return QRectF();
+}
+
+void QRunner::paint(QPainter * painter, const QStyleOptionGraphicsItem * option, QWidget * widget)
+{
+}
+
+void QRunner::advance(int phase)
+{
+}
--- PredatorSimulator/PredatorSimulator/QRunner.h
+++ PredatorSimulator/PredatorSimulator/QRunner.h
@@ -2,12 +2,38 @@
 #define Q_RUNNER_H
 
 #include "QDynamicObject.h"
+#include "Attributes.h"
 
 class QRunner : public QDynamicObject
 {
 public:
+	// TO-DO: constructeur avec des paramètres (par défaut)
 	QRunner();
 	~QRunner();
+
+	// Mutateurs
+	void setHP(int hp);
+	void setNextAttributes(int hp, QBrush const & brush);
+
+	// Accesseur
+	int getHP() const;
+
+	// Clone (fonction abstraite de QDynamicObject - doit être "overridée")
+	void clone() override;
+
+	// Fonction abstraite de QGraphicsItem (DOIT être "overridée") : retourne la taille de l'item
+	QRectF boundingRect() const override;
+	// Fonction abstraite de QGraphicsItem (DOIT être "overridée") : dessine la forme
+	void paint(QPainter * painter, const QStyleOptionGraphicsItem *option, QWidget * widget) override;
+
+	// Fonction virtuelle de QGraphicsItem (peut être "overridée") : fait un tick de simulation
+	void advance(int phase) override;
+
+private:
+	// Les points de vie
+	int mHealthPoints;
+	// Les prochains attributs
+	RunnerAttributes mNextAttributes;
 };
 
 #endif // !Q_RUNNER_H
