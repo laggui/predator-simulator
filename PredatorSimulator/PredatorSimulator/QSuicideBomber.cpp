328d75eb5d51f7ee0b7532ebb134f0f346adbadf
Guillaume Lagrange <lagrange.guillaume.1@gmail.com>
2018-12-15 14:11:55 -0500

    Added QSuicideBomber methods and attributes

--- PredatorSimulator/PredatorSimulator/QSuicideBomber.cpp
+++ PredatorSimulator/PredatorSimulator/QSuicideBomber.cpp
@@ -10,3 +10,41 @@ QSuicideBomber::QSuicideBomber()
 QSuicideBomber::~QSuicideBomber()
 {
 }
+
+void QSuicideBomber::setDamage(qreal damage)
+{
+	if (damage > 0 && damage < 1) {
+		mDamage = damage;
+	}
+	else {
+		mDamage = 0.9; // valeur par défaut si la valeur spécifiée est invalide
+	}
+}
+
+void QSuicideBomber::setNextpos(qreal x, qreal y)
+{
+	mNextPos.x = x;
+	mNextPos.y = y;
+}
+
+qreal QSuicideBomber::getDamage() const
+{
+	return mDamage;
+}
+
+void QSuicideBomber::clone()
+{
+}
+
+QRectF QSuicideBomber::boundingRect() const
+{
+	return QRectF();
+}
+
+void QSuicideBomber::paint(QPainter * painter, const QStyleOptionGraphicsItem * option, QWidget * widget)
+{
+}
+
+void QSuicideBomber::advance(int phase)
+{
+}
--- PredatorSimulator/PredatorSimulator/QSuicideBomber.h
+++ PredatorSimulator/PredatorSimulator/QSuicideBomber.h
@@ -2,12 +2,37 @@
 #define Q_SUICIDE_BOMBER_H
 
 #include "QDynamicObject.h"
+#include "Attributes.h"
 
 class QSuicideBomber : public QDynamicObject
 {
 public:
 	QSuicideBomber();
 	~QSuicideBomber();
+
+	// Mutateurs
+	void setDamage(qreal damage);
+	void setNextpos(qreal x, qreal y);
+
+	// Accesseurs
+	qreal getDamage() const;
+
+	// Clone (fonction abstraite de QDynamicObject - doit être "overridée")
+	void clone() override;
+
+	// Fonction abstraite de QGraphicsItem (DOIT être "overridée") : retourne la taille de l'item
+	QRectF boundingRect() const override;
+	// Fonction abstraite de QGraphicsItem (DOIT être "overridée") : dessine la forme
+	void paint(QPainter * painter, const QStyleOptionGraphicsItem *option, QWidget * widget) override;
+
+	// Fonction virtuelle de QGraphicsItem (peut être "overridée") : fait un tick de simulation
+	void advance(int phase) override;
+
+private:
+	// Le dommage (influence la grosseur d'un QPredator - doit être entre ]0, 1[)
+	qreal mDamage;
+	// La prochaine position
+	Position mNextPos;
 };
 
 #endif // !Q_SUICIDE_BOMBER_H
